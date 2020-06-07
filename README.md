e2d9f61ddad8ad549c7490b7fa4b60ea9f98d7d7
pudgypoppins <pudgypoppins@gmail.com>
2020-06-07 12:30:56 -0600

    b

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Abulti Apparel
 
 This is the code for the *massively* popular Abulti Apparel website
+
+## TODO:
+
+After the actual store item model is in, I'll add a "quickshop" button to both the homepage and store pages, with a modal so that users can buy stuff quicker.
--- static/js/index.js
+++ static/js/index.js
@@ -3,3 +3,15 @@
 function removeNotification(el){
 	el.parentNode.parentNode.removeChild(el.parentNode)
 }
+
+function miniShop(){
+	document.getElementById("miniShop").style.top = document.getElementsByClassName('navWrapper')[0].offsetHeight + 1 + "px";
+	//^ here I determine the top offset for the mini shop via javascript, because some devices would use different fonts, and plus it's easier on resizing
+	document.getElementById("miniShop").style.display = "block";
+	document.getElementById("new").classList.add("active");
+}
+
+function closeMiniShop(){
+	document.getElementById("miniShop").style.display = "none";
+	document.getElementById("new").classList.remove("active");
+}
