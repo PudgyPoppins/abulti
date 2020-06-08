73d2b0dc27067cc7bff7ae4ca9e53a4baed81d81
pudgypoppins <pudgypoppins@gmail.com>
2020-06-08 14:10:00 -0600

    b

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from shop.models import Item
 
 def index(request):
-    return render(request, 'home/index.html')
+	context = {
+		"popular_list": Item.objects.order_by('purchased_count')[:3], #get the top 3 most purchased items
+		"recent_list": Item.objects.order_by('-pub_date')[:3], #get the 3 most recent arrivals
+	}
+
+	return render(request, 'home/index.html', context)
 
 
